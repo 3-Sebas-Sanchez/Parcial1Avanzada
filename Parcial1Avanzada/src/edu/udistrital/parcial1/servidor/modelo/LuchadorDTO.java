26f4121eeb9cd3d25c5c324818911ecf7bf94a08
Nath <nathalycis20@gmail.com>
2026-03-23 23:20:29 -0500

    Agregando atributos luchador

--- Parcial1Avanzada/src/edu/udistrital/parcial1/servidor/modelo/LuchadorDTO.java
+++ Parcial1Avanzada/src/edu/udistrital/parcial1/servidor/modelo/LuchadorDTO.java
@@ -4,10 +4,19 @@
  */
 package edu.udistrital.parcial1.servidor.modelo;
 
+
 /**
  *
  * @author Nath
  */
 public class LuchadorDTO {
+    
+    private String nombre;
+    private double peso;
+    private int combatesGanados;
+    private String[] kimarites;
+    private LuchadorDTO rival;
+    private boolean dentroDelDohyo;
+    
     
 }
